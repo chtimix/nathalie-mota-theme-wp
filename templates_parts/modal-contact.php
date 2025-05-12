b62eb3884116aa1ff1ae08bcd3e073319899481b
chtimix <david.peugnet77@gmail.com>
2025-05-12 12:48:28 +0200

    Création du formulaire de contact avec Contact Form 7

---
+++ templates_parts/modal-contact.php
@@ -0,0 +1,11 @@
+<div class="modal-contact-overlay" style="display: none;">
+  <div class="modal-contact">
+    <button class="modal-close" aria-label="Fermer la modale">×</button>
+    
+    <div class="modal-header-image"></div>
+    
+    <div class="modal-form-container">
+      <?php echo do_shortcode('[contact-form-7 id="a84043f" title="Formulaire de contact"]'); ?>
+    </div>
+  </div>
+</div>
