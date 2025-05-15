888169bd2bc94e0a78ca3e5918f858a79f44ddc4
chtimix <david.peugnet77@gmail.com>
2025-05-15 14:46:29 +0200

    Correction header

--- header.php
+++ header.php
@@ -9,23 +9,25 @@
 <body <?php body_class(); ?>>
 
 <header class="site-header">
-    <div class="site-wrapper header-container">
-        <div class="logo">
-            <?php 
-            $logo = get_field('logo', 'option');
-            ?>
-            <a href="<?php echo esc_url(home_url('/')); ?>">
-                <img src="<?php echo $logo['url']; ?>" alt="<?php bloginfo('name'); ?>">
-            </a>
+    <div class="site-wrapper">
+        <div class="header-container">
+            <div class="logo">
+                <?php 
+                $logo = get_field('logo', 'option');
+                ?>
+                <a href="<?php echo esc_url(home_url('/')); ?>">
+                    <img src="<?php echo $logo['url']; ?>" alt="<?php bloginfo('name'); ?>">
+                </a>
+            </div>
+            <nav class="main-navigation">
+                <?php
+                    wp_nav_menu([
+                        'theme_location' => 'main_menu',
+                        'container' => false,
+                        'menu_class' => 'menu',
+                    ]);
+                ?>
+            </nav>
         </div>
-        <nav class="main-navigation">
-            <?php
-                wp_nav_menu([
-                    'theme_location' => 'main_menu',
-                    'container' => false,
-                    'menu_class' => 'menu',
-                ]);
-            ?>
-        </nav>
     </div>
 </header>
